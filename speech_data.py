c21bd9a6ef2cb57b5814da4bf9c6b9a74446d757
jeremycy <jeremycy@cmu.edu>
2016-12-04 15:50:01 -0800

    Fix extracting wav files issue

--- speech_data.py
+++ speech_data.py
@@ -61,7 +61,7 @@ def maybe_download(file, work_directory):
     # os.system('ln -s '+work_directory)
   if os.path.exists(filepath):
     print('Extracting %s to %s' % ( filepath, work_directory))
-    os.system('tar xf '+filepath)
+    os.system('tar xf '+ filepath + ' -C ' + work_directory)
   return filepath.replace(".tar","")
 
 def spectro_batch(batch_size=10):
